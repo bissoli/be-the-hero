81c3f8bc35d7e876611be70f704dac7a2711a4e6
Alison Bissoli <bissolidias@gmail.com>
2020-05-16 23:46:26 -0300

    font-end do app

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,13 +4,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from  './pages/Profile'
+import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
     return (
     <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Logon} />
-            <Route pagh="/register" component={Register} />
+            <Route path="/register" component={Register} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/incidents/new" component={NewIncident} />
         </Switch>
     </BrowserRouter>
     );
